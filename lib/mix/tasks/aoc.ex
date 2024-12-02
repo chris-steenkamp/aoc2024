4a913d9ed84f1ed43c0cd05848e7c922f819e73e
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-02 09:13:36 +0200

    Update launch config to allow debugging the app.
    Also added aoc launcher app to run Advent of Code
    solutions.

---
+++ lib/mix/tasks/aoc.ex
@@ -0,0 +1,40 @@
+defmodule Mix.Tasks.Aoc do
+  use Mix.Task
+
+  @shortdoc "Run Advent of Code solutions"
+  def run(args) do
+    case args do
+      ["sample", "1", day] ->
+        run_solution(day, :sample1)
+
+      ["sample", "2", day] ->
+        run_solution(day, :sample2)
+
+      ["part", "1", day] ->
+        run_solution(day, :part1)
+
+      ["part", "2", day] ->
+        run_solution(day, :part2)
+
+      _ ->
+        IO.puts("""
+        Usage:
+          mix aoc sample 1 [day]  - Run sample 1 for specific day
+          mix aoc sample 2 [day]  - Run sample 2 for specific day
+
+          mix aoc part 1 [day]  - Run part 1 for specific day
+          mix aoc part 2 [day]  - Run part 2 for specific day
+
+        Example:
+          mix aoc sample 1 01  - Run sample 1 for day 1
+          mix aoc part 1 01 - Run part 1 for day 1
+        """)
+    end
+  end
+
+  defp run_solution(day, part) do
+    module = String.to_existing_atom("Elixir.Day#{day}")
+    result = apply(module, part, [])
+    IO.puts("Result for Day#{day} #{part}: #{result}")
+  end
+end
