197751433d6133028514b492208d73f79afe1ae7
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-02 19:25:38 +0200

    Consolidate part1 and part2 logic into single function.

--- lib/aoc/days/day_02.ex
+++ lib/aoc/days/day_02.ex
@@ -8,45 +8,28 @@ defmodule AOC.Days.Day02 do
   def sample1(input \\ "day02/sample.txt") do
     input
     |> Helpers.get_lines()
-    |> solve_part1()
+    |> solve_parts(false)
   end
 
   def sample2(input \\ "day02/sample.txt") do
     input
     |> Helpers.get_lines()
-    |> solve_part2()
+    |> solve_parts(true)
   end
 
   def part1(input \\ "day02/input.txt") do
     input
     |> Helpers.get_lines()
-    |> solve_part1()
+    |> solve_parts(false)
   end
 
   def part2(input \\ "day02/input.txt") do
     input
     |> Helpers.get_lines()
-    |> solve_part2()
+    |> solve_parts(true)
   end
 
-  def solve_part1(lines) do
-    lines
-    |> Enum.map(fn x -> String.split(x, " ", trim: true) end)
-    |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end)
-    |> Enum.map(fn x -> Enum.zip(x, tl(x)) end)
-    |> Enum.map(fn x -> Enum.map(x, fn {a, b} -> a - b end) end)
-    |> Enum.map(fn x -> Enum.zip(x, tl(x)) end)
-    |> Enum.map(fn x ->
-      Enum.map(x, fn {a, b} ->
-        abs(a) in 1..3 and abs(b) in 1..3 and a * b > 0
-      end)
-    end)
-    |> Enum.map(fn x -> Enum.all?(x) end)
-    |> Enum.frequencies()
-    |> Map.get(true)
-  end
-
-  def solve_part2(lines) do
+  def solve_parts(lines, is_part_2) do
     lines
     |> Enum.map(fn x -> String.split(x, " ", trim: true) end)
     |> Enum.map(fn x -> Enum.map(x, fn y -> String.to_integer(y) end) end)
@@ -66,8 +49,20 @@ defmodule AOC.Days.Day02 do
         ]
       end)
     end)
-    |> Enum.map(fn x -> filter_pairs(x) end)
-    |> Enum.map(fn x -> Enum.map(x, fn [a, b] -> a or b end) end)
+    |> Enum.map(fn x ->
+      case is_part_2 do
+        true -> filter_pairs(x)
+        false -> x
+      end
+    end)
+    |> Enum.map(fn x ->
+      Enum.map(x, fn [a, b] ->
+        case is_part_2 do
+          true -> a or b
+          false -> a
+        end
+      end)
+    end)
     |> Enum.map(fn x -> Enum.all?(x) end)
     |> Enum.frequencies()
     |> Map.get(true)
