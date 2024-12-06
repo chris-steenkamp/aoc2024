893334c463454a5ef2cdfb573cbedebfb523abaf
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-06 12:20:13 +0200

    Update Helpers.get_lines with optional parameter for trimming blank lines.

--- lib/aoc/helpers.ex
+++ lib/aoc/helpers.ex
@@ -1,7 +1,7 @@
 defmodule AOC.Helpers do
-  def get_lines(filename) do
+  def get_lines(filename, trim \\ true) do
     Path.join(["priv", "inputs", filename])
     |> File.read!()
-    |> String.split(~r/\r?\n/, trim: true)
+    |> String.split(~r/\r?\n/, trim: trim)
   end
 end
