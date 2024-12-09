df9b5337beb2b5b80cf19b7be775447636965368
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-09 17:13:54 +0200

    Update day08 to use the same code for part 1 and part 2.

--- lib/aoc/days/day_08.ex
+++ lib/aoc/days/day_08.ex
@@ -37,31 +37,35 @@ defmodule AOC.Days.Day08 do
     end
   end
 
-  defp get_antinodes({p1, p2}) do
-    {dx, dy} = H.calculate_absolute_offset(p1, p2)
-
-    case H.calculate_slope(p1, p2) do
-      s when s > 0 -> [H.add_points(p1, {-dx, -dy}), H.add_points(p2, {dx, dy})]
-      s when s < 0 -> [H.add_points(p1, {dx, -dy}), H.add_points(p2, {-dx, dy})]
-      0.0 -> [H.add_points(p1, {-dx, 0}), H.add_points(p2, {dx, 0})]
-      :undefined -> [H.add_points(p1, {0, -dy}), H.add_points(p2, {0, dy})]
-    end
-  end
-
-  defp get_antinodes2({p1, p2}, n) do
+  defp get_antinodes([p1, p2], part) do
     {dx, dy} = H.calculate_absolute_offset(p1, p2)
     slope = H.calculate_slope(p1, p2)
 
-    Stream.iterate({p1, p2}, fn {p1, p2} ->
-      case slope do
-        s when s > 0 -> {H.add_points(p1, {-dx, -dy}), H.add_points(p2, {dx, dy})}
-        s when s < 0 -> {H.add_points(p1, {dx, -dy}), H.add_points(p2, {-dx, dy})}
-        0.0 -> {H.add_points(p1, {-dx, 0}), H.add_points(p2, {dx, 0})}
-        :undefined -> {H.add_points(p1, {0, -dy}), H.add_points(p2, {0, dy})}
+    n =
+      cond do
+        part == :part1 -> 2
+        true -> 26
       end
-    end)
-    |> Enum.take(n + 1)
-    |> Enum.flat_map(fn {p1, p2} -> [p1, p2] end)
+
+    nodes =
+      Stream.iterate({p1, p2}, fn {p1, p2} ->
+        case slope do
+          s when s > 0 -> {H.add_points(p1, {-dx, -dy}), H.add_points(p2, {dx, dy})}
+          s when s < 0 -> {H.add_points(p1, {dx, -dy}), H.add_points(p2, {-dx, dy})}
+          0.0 -> {H.add_points(p1, {-dx, 0}), H.add_points(p2, {dx, 0})}
+          :undefined -> {H.add_points(p1, {0, -dy}), H.add_points(p2, {0, dy})}
+        end
+      end)
+      |> Enum.take(n)
+      |> Enum.flat_map(fn {p1, p2} -> [p1, p2] end)
+
+    cond do
+      part == :part1 ->
+        Enum.drop(nodes, 2)
+
+      true ->
+        nodes
+    end
   end
 
   defp pair([a | rest]) do
@@ -71,8 +75,8 @@ defmodule AOC.Days.Day08 do
 
         # always ensure that p1 is above p2
         cond do
-          y1 > y2 -> {b, a}
-          true -> {a, b}
+          y1 > y2 -> [b, a]
+          true -> [a, b]
         end
       end)
 
@@ -91,7 +95,7 @@ defmodule AOC.Days.Day08 do
     |> map_grid_chars()
     |> Enum.map(fn {_, v} -> MapSet.to_list(v) end)
     |> Enum.flat_map(&pair/1)
-    |> Enum.flat_map(&get_antinodes/1)
+    |> Enum.flat_map(&get_antinodes(&1, :part1))
     |> Enum.filter(fn x -> H.in_bounds?(grid, x) end)
     |> Enum.uniq()
     |> Enum.count()
@@ -102,7 +106,7 @@ defmodule AOC.Days.Day08 do
     |> map_grid_chars()
     |> Enum.map(fn {_, v} -> MapSet.to_list(v) end)
     |> Enum.flat_map(&pair/1)
-    |> Enum.flat_map(&get_antinodes2(&1, 25))
+    |> Enum.flat_map(&get_antinodes(&1, :part2))
     |> Enum.filter(fn x -> H.in_bounds?(grid, x) end)
     |> Enum.uniq()
     |> Enum.count()
