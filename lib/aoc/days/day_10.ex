0877fbf6421dc8a2be35c57e7b3380c765cebb92
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-11 20:28:30 +0200

    Refactor and simplify day 10 part 2

--- lib/aoc/days/day_10.ex
+++ lib/aoc/days/day_10.ex
@@ -72,16 +72,6 @@ defmodule AOC.Days.Day10 do
           H.at(grid, {x, y}) == "0",
           do: {x, y}
 
-    directions = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
-
-    # moves =
-    #   directions
-    #   |> Enum.map(&H.add_points(p, &1))
-    #   |> Enum.filter(&H.in_bounds?(grid, &1))
-    #   |> Enum.filter(fn p2 ->
-    #     String.to_integer(H.at(grid, p2)) - String.to_integer(H.at(grid, p)) === 1
-    #   end)
-
     next = fn f, grid, p, height, path ->
       cond do
         H.out_of_bounds?(grid, p) ->
@@ -97,40 +87,21 @@ defmodule AOC.Days.Day10 do
           path
 
         true ->
-          p2 = []
-
-          p2 =
-            if (temp = f.(f, grid, H.add_points(p, {0, -1}), height + 1, [p | path])) != [],
-              do: temp ++ p2,
-              else: p2
-
-          p2 =
-            if (temp = f.(f, grid, H.add_points(p, {0, 1}), height + 1, [p | path])) != [],
-              do: temp ++ p2,
-              else: p2
-
-          p2 =
-            if (temp = f.(f, grid, H.add_points(p, {-1, 0}), height + 1, [p | path])) != [],
-              do: temp ++ p2,
-              else: p2
-
-          p2 =
-            if (temp = f.(f, grid, H.add_points(p, {1, 0}), height + 1, [p | path])) != [],
-              do: temp ++ p2,
-              else: p2
-
-          p2
+          @directions
+          |> Enum.map(&H.add_points(p, &1))
+          |> Enum.reduce([], fn p, acc ->
+            if (temp = f.(f, grid, p, height + 1, [p | path])) != [],
+              do: temp ++ acc,
+              else: acc
+          end)
       end
     end
 
-    paths =
-      trailheads
-      |> Enum.flat_map(fn t ->
-        next.(next, grid, t, 0, [])
-      end)
-      |> Enum.count()
-      |> div(9)
-
-    paths
+    trailheads
+    |> Enum.flat_map(fn t ->
+      next.(next, grid, t, 0, [])
+    end)
+    |> Enum.count()
+    |> div(9)
   end
 end
