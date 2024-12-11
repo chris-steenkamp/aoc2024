1620ca685b37d7ab36eb1a55bf11ff7b94b010e1
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-11 09:28:47 +0200

    Create a min heap implementation

---
+++ lib/DataStructures/minheap.ex
@@ -0,0 +1,83 @@
+# Implementation of a Min-Heap in Elixir
+# A min-heap is a binary tree where each parent node is smaller than or equal to its child nodes.
+# We will use a list to represent the heap, where the first element is `nil` (to simplify calculations for indices).
+
+defmodule DataStructures.MinHeap do
+  # Initializes an empty heap
+  def new do
+    []
+  end
+
+  # Inserts an element into the heap
+  def insert(heap, value) do
+    # Append the value to the heap
+    heap = heap ++ [value]
+    # Bubble up the newly added value to restore the min-heap property
+    bubble_up(heap, length(heap) - 1)
+  end
+
+  # Helper function: Bubble up the element at the given index
+  defp bubble_up(heap, index) when index == 0, do: heap
+
+  defp bubble_up(heap, index) do
+    parent_index = div(index - 1, 2)
+
+    if Enum.at(heap, parent_index) > Enum.at(heap, index) do
+      heap = swap(heap, parent_index, index)
+      bubble_up(heap, parent_index)
+    else
+      heap
+    end
+  end
+
+  # Extracts the minimum element (root of the heap)
+  def extract_min([]), do: {:error, "Heap is empty"}
+
+  def extract_min(heap) do
+    case heap do
+      [min] ->
+        {min, []}
+
+      _ ->
+        # Extract the root (minimum element)
+        min = hd(heap)
+        # Move the last element to the root
+        last = List.last(heap)
+        new_heap = List.replace_at(heap, 0, last) |> Enum.drop(-1)
+        # Restore heap property
+        sifted_heap = sift_down(new_heap, 0)
+        {min, sifted_heap}
+    end
+  end
+
+  # Helper function: Sift down the element at the given index
+  defp sift_down(heap, index) do
+    left_child = 2 * index + 1
+    right_child = 2 * index + 2
+    smallest = index
+
+    smallest =
+      if left_child < length(heap) && Enum.at(heap, left_child) < Enum.at(heap, smallest),
+        do: left_child,
+        else: smallest
+
+    smallest =
+      if right_child < length(heap) && Enum.at(heap, right_child) < Enum.at(heap, smallest),
+        do: right_child,
+        else: smallest
+
+    if smallest != index do
+      heap = swap(heap, index, smallest)
+      sift_down(heap, smallest)
+    else
+      heap
+    end
+  end
+
+  # Helper function: Swap two elements in the heap
+  defp swap(heap, i, j) do
+    heap
+    |> List.replace_at(i, Enum.at(heap, j))
+    |> List.replace_at(j, Enum.at(heap, i))
+  end
+end
---
+++ test/minheap_test.exs
@@ -0,0 +1,61 @@
+defmodule AOC.Test.MinHeapTest do
+  alias DataStructures.MinHeap
+
+  use ExUnit.Case
+
+  @moduletag :datastructures
+
+  test "create a new heap" do
+    assert MinHeap.new() == []
+  end
+
+  test "insert elements into the heap" do
+    heap = MinHeap.new()
+    heap = MinHeap.insert(heap, 5)
+    heap = MinHeap.insert(heap, 3)
+    heap = MinHeap.insert(heap, 8)
+
+    assert heap == [3, 5, 8]
+  end
+
+  test "extract the minimum element" do
+    heap = MinHeap.new()
+    heap = MinHeap.insert(heap, 5)
+    heap = MinHeap.insert(heap, 3)
+    heap = MinHeap.insert(heap, 8)
+    {min, heap} = MinHeap.extract_min(heap)
+
+    assert min == 3
+    assert heap == [5, 8]
+  end
+
+  test "extract minimum from an empty heap" do
+    assert MinHeap.extract_min(MinHeap.new()) == {:error, "Heap is empty"}
+  end
+
+  test "complex heap operations" do
+    heap = MinHeap.new()
+    heap = MinHeap.insert(heap, 10)
+    heap = MinHeap.insert(heap, 4)
+    heap = MinHeap.insert(heap, 15)
+    heap = MinHeap.insert(heap, 1)
+    heap = MinHeap.insert(heap, 7)
+
+    assert heap == [1, 4, 15, 10, 7]
+
+    {min, heap} = MinHeap.extract_min(heap)
+    assert min == 1
+    assert heap == [4, 7, 15, 10]
+
+    {min, heap} = MinHeap.extract_min(heap)
+    assert min == 4
+    assert heap == [7, 10, 15]
+
+    {min, heap} = MinHeap.extract_min(heap)
+    assert min == 7
+    assert heap == [10, 15]
+
+    heap = MinHeap.insert(heap, 3)
+    assert heap == [3, 15, 10]
+  end
+end
