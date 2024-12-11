48f560e917a523dcfff8e40ccbf6ddde0ad87d95
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-12-11 19:17:15 +0200

    Refactor the code to use private methods more funtional approach to next position generation.

--- lib/aoc/days/day_10.ex
+++ lib/aoc/days/day_10.ex
@@ -1,6 +1,8 @@
 defmodule AOC.Days.Day10 do
   alias AOC.Helpers, as: H
 
+  @directions [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
+
   def sample1(input \\ "day10/sample.txt") do
     input
     |> H.get_lines()
@@ -25,6 +27,27 @@ defmodule AOC.Days.Day10 do
     |> solve_part2()
   end
 
+  defp next(grid, p, height) do
+    cond do
+      H.out_of_bounds?(grid, p) ->
+        MapSet.new()
+
+      H.at(grid, p) === "." ->
+        MapSet.new()
+
+      String.to_integer(H.at(grid, p)) !== height ->
+        MapSet.new()
+
+      height === 9 ->
+        MapSet.new([p])
+
+      true ->
+        @directions
+        |> Enum.map(&H.add_points(p, &1))
+        |> Enum.reduce(MapSet.new(), &MapSet.union(&2, next(grid, &1, height + 1)))
+    end
+  end
+
   def solve_part1(grid) do
     {width, height} = H.get_dimensions(grid)
 
@@ -34,47 +57,8 @@ defmodule AOC.Days.Day10 do
           H.at(grid, {x, y}) == "0",
           do: {x, y}
 
-    # directions = [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
-
-    # moves =
-    #   directions
-    #   |> Enum.map(&H.add_points(p, &1))
-    #   |> Enum.filter(&H.in_bounds?(grid, &1))
-    #   |> Enum.filter(fn p2 ->
-    #     String.to_integer(H.at(grid, p2)) - String.to_integer(H.at(grid, p)) === 1
-    #   end)
-
-    next = fn f, grid, p, height ->
-      cond do
-        H.out_of_bounds?(grid, p) ->
-          MapSet.new()
-
-        H.at(grid, p) === "." ->
-          MapSet.new()
-
-        String.to_integer(H.at(grid, p)) !== height ->
-          MapSet.new()
-
-        height === 9 ->
-          MapSet.new([p])
-
-        true ->
-          nines = MapSet.new()
-          nines = MapSet.union(nines, f.(f, grid, H.add_points(p, {0, -1}), height + 1))
-          nines = MapSet.union(nines, f.(f, grid, H.add_points(p, {0, 1}), height + 1))
-          nines = MapSet.union(nines, f.(f, grid, H.add_points(p, {-1, 0}), height + 1))
-          nines = MapSet.union(nines, f.(f, grid, H.add_points(p, {1, 0}), height + 1))
-          nines
-      end
-    end
-
     trailheads
-    |> Enum.map(fn start ->
-      start
-    end)
-    |> Enum.map(fn t ->
-      next.(next, grid, t, 0)
-    end)
+    |> Enum.map(fn t -> next(grid, t, 0) end)
     |> Enum.map(&MapSet.size(&1))
     |> Enum.sum()
   end
